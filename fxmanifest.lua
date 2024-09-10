f628e483f86b88c56e4832a59d01434a853bfcf2
Vasia92LT <aurimas.buzas1992@gmail.com>
2024-09-10 21:10:14 +0300

    Speedometer FiveM Qbcore
    De-V-altu

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5' 
+
+author '7evenStudios'
+description 'Speedometer Standalone'
+version '1.0'
+
+client_scripts {
+	"client.lua",
+	"belts.lua"
+}
+
+ui_page {
+	'html/index.html'
+}
+
+files {
+	'html/*.png',
+	'html/*.html',
+	'html/index.html',
+	"html/css/Overlay.css",
+	'html/img/*.svg',
+	'html/fonts/*.ttf',
+	'html/js/*.js',
+}
